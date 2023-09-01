b96680056095046f4c00a3dc296e473a38cbab66
Redmomn <109732988+Redmomn@users.noreply.github.com>
2023-09-01 20:23:29 +0800

    test

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,78 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+    // 站点配置
+    lang: 'zh-CN',
+    title: 'go-cqhttp 帮助中心',
+    description: 'Onebot 的 golang 实现，轻量、原生跨平台',
+    base: '/',
+    head:[
+        ['link', { rel: 'icon', href: 'https://www.lovenie.icu/blog/wp-content/uploads/2023/09/宁宁.jpg' }]
+    ],
+    plugins: [
+        ['@vuepress/back-to-top'],
+        [
+            '@vuepress/pwa',
+            {
+                skipWaiting: true
+            }
+        ],
+        [
+            '@vuepress/plugin-pwa-popup',
+            {
+                locales: {
+                    '/': {
+                        message: '发现新内容可用',
+                        buttonText: '刷新',
+                    }
+                }
+            }
+        ],
+        [
+            '@vuepress/plugin-google-analytics',
+            {
+                id: 'G-0PQPMKMWLP'
+            }
+        ],
+        [
+            "@vuepress/plugin-docsearch",
+            {
+                apiKey: '4cfe0f33be16c346fed77effff922cf8',
+                indexName: 'go-cqhttp',
+                algoliaOptions: {
+                    facetFilters: ["lang:en-US"],
+                },
+            }
+        ]
+    ],
+
+    // 主题和它的配置
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+        repo: 'Mrs4s/go-cqhttp',
+        docsRepo: 'ishkong/go-cqhttp-docs',
+        docsBranch: 'main',
+        docsDir: 'docs',
+        editLinkText: "编辑此页",
+        lastUpdated: true,
+        lastUpdatedText: '上一次编辑',
+        contributors: true,
+        tip: "提示",
+        warning: "注意",
+        danger: "警告",
+        backToHome: "返回首页",
+        navbar: [
+            { text: 'Guide', children: ['/guide/', '/guide/quick_start.md', '/guide/config.md', '/guide/eventfilter.md', '/guide/file.md', '/guide/achieve.md', '/guide/docker.md'] },
+            { text: 'API', children: ['/api/', '/api/guild.md'] },
+            { text: 'Event', children: ['/event/', '/event/guild.md'] },
+            { text: 'CQ Code', link: '/cqcode/' },
+            { text: 'Guild', link: '/guild/'},
+            { text: 'Reference', children: ['/reference/', '/reference/data_struct.md'] },
+            { text: 'Advanced', link: '/advanced/'},
+            { text: 'FAQ', link: '/faq/' }
+        ],
+        sidebar: "auto",
+    },
+
+})
---
+++ docs/README.md
@@ -0,0 +1,23 @@
+---
+home: true
+heroImage: https://www.lovenie.icu/blog/wp-content/uploads/2023/09/宁宁2.png
+tagline: 夹带私货，吹一波
+
+actions:
+  - text: 仓库地址
+    type: secondary
+    link: https://github.com/Mrs4s/Redmomn/test-docs
+  - text: 快速上手🚀
+    type: primary
+    link: /guide/
+
+features:
+- title: 简洁至上
+  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
+- title: Vue驱动
+  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
+- title: 高性能
+  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+
+footer: CC-BY-SA-4.0 Licensed | Copyright © 2021-present Redmomn
+---
